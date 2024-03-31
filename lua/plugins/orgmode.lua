449ba8f11d96a4a83b5a3689395764c32f485252
Jan Fiedler <jbf@zuloo.de>
2024-03-31 16:01:23 +0200

    add plugins orgmode and obsidian

---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,40 @@
+return { 
+  {
+    'nvim-orgmode/orgmode',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+    },
+    event = 'VeryLazy',
+    config = function()
+      -- Load treesitter grammar for org
+      require('orgmode').setup_ts_grammar()
+
+      -- Setup treesitter
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+        },
+        ensure_installed = { 'org' },
+      })
+
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/Documents/git/org/**/*',
+        org_default_notes_file = '~/Documents/git/org/refile.org',
+      })
+
+      -- Setup Conceal 
+      local api, opt = vim.api, vim.opt
+      api.nvim_create_autocmd(
+        "FileType",
+        {
+          pattern = "org",
+          callback = function()
+            opt.conceallevel = 2
+            opt.concealcursor = 'nc'
+          end
+        }
+      )
+    end,
+  }
+}
