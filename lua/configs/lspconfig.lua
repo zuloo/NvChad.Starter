266c029af20eae20811487af8512ddbaf6b6ec93
Jan Fiedler <jbf@zuloo.de>
2024-04-04 01:06:50 +0200

    lots of LSPs to choose and biome ftw

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,75 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+
+  "cssls",
+  -- "stylelint-lsp",
+  -- "css-variables-language-server",
+  -- "cssmodules-language-server",
+  --
+  -- "prettier",
+  "biome",
+  --
+  -- "angular-language-server",
+  -- "nextls",
+  -- "svelte-language-server",
+  -- "custom-elements-language-server", -- web components
+  -- -- "tailwindcss-language-server",
+  -- "rustywind", -- tailwind in rust
+  -- "unocss-language-server",
+  --
+  -- "blue", -- like black but softer
+  -- "debugpy",
+  -- "djlint", -- Jinja, Django, Handlebars, golang templates
+  -- "docformatter", -- docstring pep 257
+  -- "pyment", -- docstring generator
+  -- "pyre", -- pep 484 complient
+  --
+  -- "jq-lsp",
+  -- "json-lsp",
+  -- -- "spectral-language-server", -- json/yaml \w OpenAPI support
+  -- "vacuum",
+  -- "taplo", -- toml lsp
+  --
+  -- "remark-language-server",
+  -- "markdown-oxide",
+  -- "ltex-ls",
+  --
+  -- "typst-lsp",
+  -- "typstfmt",
+  --
+  -- "nil",
+  -- "nixpkgs-fmt",
+  --
+  -- "ansible-language-server",
+  -- "ansible-lint",
+  --
+  -- "terraform-ls",
+  -- "tflint",
+  -- "tfsec", -- security scanner
+  --
+  -- "nginx-language-server",
+  -- "systemdlint",
+  --
+  -- "gitleaks", -- check for secrets in git
+  --
+  -- "reformat-gherkin",
+  --
+  -- "snyk-ls",
+  -- "sonarlint-language-server",
+  -- "trivy",
+  -- "trufflehog",
+  --
+  -- "openscad-lsp",
+  --
+  -- "yls-yara",
+  --
+  -- "woke",
+  -- "write-good",
+  -- "typos",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
