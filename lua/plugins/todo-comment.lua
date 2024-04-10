e019d763c600790eb2dc7426d989649824a2d3b4
Jan Fiedler <jbf@zuloo.de>
2024-04-10 11:44:25 +0200

    move opts to setup function

--- lua/plugins/todo-comment.lua
+++ lua/plugins/todo-comment.lua
@@ -17,5 +17,33 @@ return {
     end, { desc = "Previous todo comment" })
 
     todo_comments.setup()
-  end,
+    todo_comments.setup({
+      keywords = {
+        FIX = {
+          -- icon used for the sign, and in search results
+          icon = " ",
+          -- can be a hex color, or a named color (see below)
+          color = "error",
+          -- a set of other keywords that all map to this FIX keywords
+          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+          -- configure signs for some keywords individually
+          -- signs = false,
+        },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      },
+      colors = {
+        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+        info = { "DiagnosticInfo", "#2563EB" },
+        hint = { "DiagnosticHint", "#10B981" },
+        default = { "IdentifierDefault", "#7C3AED" },
+        test = { "IdentifierTest", "#FF00FF" }
+      },
+    })
+  end
 }
